ba3251eb29f247bf2332c278ab2abfe529d10594
Andrii Rudnik <andrii_rudnik@epam.com>
2022-10-18 14:52:07 +0300

    added lombok dependency. Changed code to use the Lombok annotations

--- Week 4/Solution/src/main/java/com/epam/planes/MilitaryPlane.java
+++ Week 4/Solution/src/main/java/com/epam/planes/MilitaryPlane.java
@@ -1,45 +1,20 @@
 package com.epam.planes;
 
-import java.util.Objects;
 import com.epam.constants.MilitaryType;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@EqualsAndHashCode(callSuper=true)
+@ToString(callSuper=true)
 public class MilitaryPlane extends Plane{
 
-    private MilitaryType militaryPlaneType;
+    @Getter private MilitaryType militaryPlaneType;
 
     public MilitaryPlane(String planeModel, int maximumPlaneSpeed, int maximumPlaneFlightDistance,
     		int maximumPlaneLoadCapacity, MilitaryType militaryPlaneType) {
         super(planeModel, maximumPlaneSpeed, maximumPlaneFlightDistance, maximumPlaneLoadCapacity);
         this.militaryPlaneType = militaryPlaneType;
-    }
-
-    public MilitaryType getType() {
-        return militaryPlaneType;
-    }
-
-    @Override
-    public String toString() {
-        return super.toString().replace("}", ", type=" + militaryPlaneType + '}');
-    }
-
-    @Override
-    public boolean equals(Object o) {
-    	
-        if (this == o) {
-        	return true;
-        }
-        
-        if (!(o instanceof MilitaryPlane) || !super.equals(o)) {
-        	return false;
-        }
-        
-        MilitaryPlane plane = (MilitaryPlane) o;
-        
-        return militaryPlaneType == plane.militaryPlaneType;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), militaryPlaneType);
     }
 }
--- Week 4/Solution/src/main/java/com/epam/planes/PassengerPlane.java
+++ Week 4/Solution/src/main/java/com/epam/planes/PassengerPlane.java
@@ -1,44 +1,18 @@
 package com.epam.planes;
 
-import java.util.Objects;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 
+@EqualsAndHashCode(callSuper=true)
+@ToString(callSuper=true)
 public class PassengerPlane extends Plane {
 
-	private int maximumPlanePassengerCapacity;
+	@Getter private int maximumPlanePassengerCapacity;
 
 	public PassengerPlane(String planeModel, int maximumPlaneSpeed, int maximumPlaneFlightDistance, 
 			int maximumPlaneLoadCapacity, int maximumPlanePassengerCapacity) {
 		super(planeModel, maximumPlaneSpeed, maximumPlaneFlightDistance, maximumPlaneLoadCapacity);
 		this.maximumPlanePassengerCapacity = maximumPlanePassengerCapacity;
-	}
-
-	public int getPassengersCapacity() {
-		return maximumPlanePassengerCapacity;
-	}
-
-	@Override
-	public String toString() {
-		return super.toString().replace("}", ", passengersCapacity=" 
-			+ maximumPlanePassengerCapacity + '}');
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) {
-			return true;
-		}
-		
-		if (!(o instanceof PassengerPlane) || !super.equals(o)) {
-			return false;
-		}
-		
-		PassengerPlane plane = (PassengerPlane) o;
-		
-		return maximumPlanePassengerCapacity == plane.maximumPlanePassengerCapacity;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(super.hashCode(), maximumPlanePassengerCapacity);
 	}
 }
