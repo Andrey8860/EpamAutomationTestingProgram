73321abfc59aa54de7d489e4ff7be639743936f0
Andrii Rudnik <andrii_rudnik@epam.com>
2022-10-27 10:19:19 +0300

    submitted an initial solution to week 7

---
+++ Week 7/solution/src/test/java/com/epam/week7/pages/SportchekHomePage.java
@@ -0,0 +1,16 @@
+package com.epam.week7.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class SportchekHomePage extends BasePage {
+	
+	private static final String pageURL = "https://qa3-www.sportchek.ca/en.html";
+	
+	public SportchekHomePage(WebDriver driver) {
+		super(driver);
+	}
+	
+	public void openPage() {
+		driver.get(pageURL);
+	}
+}
