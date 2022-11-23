60d531eacd50f1cf7fc3c078033c5115697237a7
Andrii Rudnik <andrii_rudnik@epam.com>
2022-11-23 11:54:06 +0200

    submitted an initial solution to week 10

---
+++ Week 10/solution/src/test/java/com/epam/week10/model/Email.java
@@ -0,0 +1,19 @@
+package com.epam.week10.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@EqualsAndHashCode
+@ToString
+@NoArgsConstructor
+public class Email {
+	
+	@Getter private String emailAddress;
+	
+	public Email setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+		return this;
+	}
+}
---
+++ Week 10/solution/src/test/java/com/epam/week10/util/TestListener.java
@@ -0,0 +1,21 @@
+package com.epam.week10.util;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import com.epam.week10.service.ScreenshotTaker;
+
+public class TestListener implements ITestListener {
+
+	private final static String PASSED_SCREENSHOTS_PATH = ".//target/screenshots/passed/";
+	private final static String FAILED_SCREENSHOTS_PATH = ".//target/screenshots/failed/";
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		ScreenshotTaker.saveScreenshot(PASSED_SCREENSHOTS_PATH);
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		ScreenshotTaker.saveScreenshot(FAILED_SCREENSHOTS_PATH);
+	}
+}
