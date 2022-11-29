123259c70d1a393773f29b92db5a8cc3ecd4c5f1
Andrii Rudnik <andrii_rudnik@epam.com>
2022-11-29 10:50:16 +0200

    resolved the issues. Added a main properties file

--- Week 9/solution/src/test/java/com/epam/week9/service/EmailCreator.java
+++ Week 9/solution/src/test/java/com/epam/week9/service/EmailCreator.java
@@ -4,10 +4,6 @@ import com.epam.week9.model.Email;
 
 public class EmailCreator {
 	
-	public static Email emptyEmail() {
-		return new Email();
-	}
-	
 	public static Email specificEmail(String emailAddress) {
 		return new Email().setEmailAddress(emailAddress);
 	}
